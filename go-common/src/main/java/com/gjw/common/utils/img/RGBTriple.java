a632967258ab9e46308b313d9c95811d7b130ba7
gaojunwei1 <gaojunwei@jd.com>
2019-09-18 16:01:42 +0800

    抖动算法处理设备显示的图片

---
+++ go-common/src/main/java/com/gjw/common/utils/img/RGBTriple.java
@@ -0,0 +1,17 @@
+package com.gjw.common.utils.img;
+
+/**
+ * @author gaojunwei
+ * @date 2019/9/17 13:31
+ */
+public class RGBTriple {
+    public final byte[] channels;
+
+    public RGBTriple(){
+        channels = new byte[3];
+    }
+
+    public RGBTriple(int R, int G, int B){
+        channels = new byte[]{(byte)R, (byte)G, (byte)B};
+    }
+}
