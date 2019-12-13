a6599cfb0138657db2a0244889cc7fe72bc75e89
gaojunwei1 <gaojunwei@jd.com>
2019-12-13 19:51:36 +0800

    Apriori

--- go-common/src/test/java/com.test/TestCase.java
+++ go-common/src/test/java/com.test/TestCase.java
@@ -2,12 +2,14 @@ package com.test;
 
 import com.gjw.common.utils.img.BMPConverter;
 import com.gjw.common.utils.img.enums.DeviceTypeEnums;
+import lombok.Data;
 import org.junit.Test;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
 
 /**
  * @author gaojunwei
@@ -20,5 +22,42 @@ public class TestCase {
         BufferedImage bufferedImage = ImageIO.read(new File(imgFilePathStr));
         BufferedImage bufferedImage1 = BMPConverter.floyd(bufferedImage, DeviceTypeEnums.DEVICE_BW);
         ImageIO.write(bufferedImage1, "png", new File("C:\\Users\\gaojunwei\\Pictures\\gx\\hehe2.png"));
+    }
+
+    @Test
+    public void  test002(){
+        List<User> list1 = new ArrayList<>();
+        User user1 = new User(1,"gjw");
+        User user2 = new User(2,"gjw");
+
+        list1.add(user1);
+        list1.add(user2);
+
+        List<User> list2 = new ArrayList<>();
+
+        User user3 = new User(1,"gjw");
+        User user4 = new User(2,"gjw");
+
+        list2.add(user3);
+        list2.add(user4);
+
+        System.out.println(list1.equals(list2));
+        System.out.println(list1==list2);
+        System.out.println();
+        System.out.println(user1.equals(user3));
+        System.out.println(user2.equals(user4));
+        System.out.println(user1==user3);
+        System.out.println(user2==user4);
+    }
+
+    @Data
+    class User{
+        Integer age;
+        String name;
+
+        public User(Integer age,String name){
+            this.age = age;
+            this.name = name;
+        }
     }
 }
