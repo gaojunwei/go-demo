99f6553c8646487f09e34e775ab69ab6bbaf7c35
gaojunwei <gjw.5@qq.com>
2020-02-25 09:04:09 +0800

    thymeleaf模板支持

--- go-api/src/main/java/com/gjw/common/innovation/controller/IndexController.java
+++ go-api/src/main/java/com/gjw/common/innovation/controller/IndexController.java
@@ -2,24 +2,50 @@ package com.gjw.common.innovation.controller;
 
 import com.gjw.common.enums.SystemCodeEnums;
 import com.gjw.common.result.BasicResult;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: gaojunwei
  * @Date: 2019/7/2 19:53
  * @Description:
  */
-@RestController
+@Controller
 @RequestMapping("/")
+@Slf4j
 public class IndexController {
-    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping("")
-    public BasicResult index(){
+    public String index(Model model){
+        model.addAttribute("msg", "Hello World!");
+        Map<String,String> data1 = new HashMap<>();
+        data1.put("name","小明");
+        data1.put("age","12");
+
+        Map<String,String> data2 = new HashMap<>();
+        data2.put("name","钢蛋");
+        data2.put("age","10");
+
+        model.addAttribute("list", Arrays.asList(data1,data2));
+        logger.info("返回页面");
+        return "index";
+    }
+
+    @RequestMapping("/index")
+    @ResponseBody
+    public BasicResult test(){
         BasicResult result = BasicResult.instance(SystemCodeEnums.SUCCESS.getCode(),SystemCodeEnums.SUCCESS.getMsg());
+        logger.info("返回JSON格式数据");
         return result;
     }
 }
--- go-api/src/main/java/com/gjw/common/innovation/repository/RenqunRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gjw.common.innovation.repository;
-
-import com.gjw.common.innovation.repository.domain.Renqun;
-import com.gjw.common.innovation.repository.domain.TPower;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface RenqunRepository extends JpaRepository<Renqun, Long>,JpaSpecificationExecutor<Renqun> {
-}
