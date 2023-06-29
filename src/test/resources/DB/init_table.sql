4ad2928d1f3b31af7ae4bbd86082664b3553b13a
gaojunwei <gjw.5@qq.com>
2023-06-29 18:02:27 +0800

    mybatis-plus配置维护

--- src/test/resources/DB/init_table.sql
+++ src/test/resources/DB/init_table.sql
@@ -1,9 +1,8 @@
 DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE `user_info` (
-	user_id BIGINT UNSIGNED NOT NULL COMMENT '用户ID',
-	user_name varchar(100) NOT NULL COMMENT '用户名称',
-	user_age INT DEFAULT -1 NULL COMMENT '用户年龄',
-	from_source varchar(20) NULL COMMENT '用户来源',
-	CONSTRAINT user_info_pk PRIMARY KEY (user_id)
-)
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户信息';
+    `user_id` bigint unsigned NOT NULL COMMENT '用户ID',
+    `user_name` varchar(100) NOT NULL COMMENT '用户名称',
+    `user_age` int unsigned DEFAULT NULL COMMENT '用户年龄',
+    `from_source` varchar(20) DEFAULT NULL COMMENT '用户来源',
+    PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户信息'
