8e76e84ca6cc9f06836f7cad4fc3445bac40d6f3
gaojunwei <gjw.5@qq.com>
2023-06-29 11:30:37 +0800

    kotlin集成 mapstruct

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# just
+# kotlin springboot项目
 
 ```text
 {"age":18,"clzz":"初一三班","name":"小明","school":"中山大学","teacher":"刘能"}
 http://127.0.0.1:5670/st
+```
+## kotlin集成mapstruct
+参考项目地址：https://github.com/mapstruct/mapstruct-examples
+问题记录
+```text
+ 问题一：
+    @Mappings 参数不能嵌套 @Mapping，只能通过写多个 @Mapping 实现；
 ```
--- src/test/kotlin/com/test/service/MainTest.kt
+++ src/test/kotlin/com/test/service/MainTest.kt
@@ -3,16 +3,10 @@ package com.test.service
 import com.alibaba.fastjson2.JSON
 import com.gjw.go.excel.listener.student.StudentImpDto
 import com.gjw.go.mapstruct.StudentMapper
-import org.mapstruct.factory.Mappers
 import java.time.LocalDateTime
 import kotlin.test.Test
 
-//https://www.baeldung.com/kotlin/mapstruct-data-classes
-//https://juejin.cn/s/kotlin%20mapstruct%20maven
-//https://github.com/mapstruct/mapstruct-examples
-
-
-class MainTestKt{
+class MainTestKt {
     @Test
     fun mainssss() {
         var studentImpDto = StudentImpDto().apply {
@@ -24,8 +18,8 @@ class MainTestKt{
             createTime = LocalDateTime.now()
         }
 
-        println("studentImpDto = "+JSON.toJSONString(studentImpDto))
+        println("studentImpDto = " + JSON.toJSONString(studentImpDto))
         var studentPo = StudentMapper.INSTANCE.studentImpDtoToStudentPo(studentImpDto)
-        println("studentPo = "+JSON.toJSONString(studentPo))
+        println("studentPo = " + JSON.toJSONString(studentPo))
     }
 }
