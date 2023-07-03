4c54807bf64d7eae7de254f09ff071e7880c10b3
gaojunwei <gjw.5@qq.com>
2023-07-03 17:37:55 +0800

    flowable spring集成测试

---
+++ src/test/kotlin/com/gjw/go/common/utils/SpringUtilsTest.kt
@@ -0,0 +1,34 @@
+package com.gjw.go.common.utils
+
+import com.gjw.go.service.EasyExcelService
+import com.gjw.go.AbstractSpringTest
+import kotlin.test.Test
+
+class SpringUtilsTest : AbstractSpringTest() {
+
+    @Test
+    fun test() {
+        SpringUtils.getBean<EasyExcelService>("easyExcelServiceImpl").listAll()
+        Thread.sleep(1000)
+        SpringUtils.getBean<EasyExcelService>("myExcelServiceImp").listAll()
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun test33() {
+        SpringUtils.getBean("easyExcelServiceImpl", EasyExcelService::class.java)?.listAll()
+        Thread.sleep(1000)
+        SpringUtils.getBean("myExcelServiceImp", EasyExcelService::class.java)?.listAll()
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun test3ee3() {
+        var list = SpringUtils.getBeansOfType(EasyExcelService::class.java)
+        list.values.forEach {
+            it.listAll()
+            println("---------")
+        }
+    }
+
+}
